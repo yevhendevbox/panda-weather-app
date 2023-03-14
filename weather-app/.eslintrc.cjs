7187cf5510fd9bef1084bdfc94eebdeaf6232326
Yevhen Dovhan <y.d.devbox@gmail.com>
2023-03-14 18:52:32 +0200

    Initial commit

---
+++ weather-app/.eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'max-len': ['error', { code: 120, tabWidth: 2 }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-useless-escape': 'off',
+  },
+}
